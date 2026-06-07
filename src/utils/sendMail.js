943536505439dc2c0a5182521a0f52730da8e00e
Denis Hrytsin <pleyden8@gmail.com>
2026-06-07 19:56:40 +0300

    add tomeout

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -3,10 +3,14 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 5000,
+  socketTimeout: 10000,
 });
 
 export const sendEmail = async ({ from, to, subject, html }) => {
